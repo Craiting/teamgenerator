e93f9e7247a6d785d2d8764e0789baaba5695025
Craig Tingey <craig@lightningkite.com>
2016-10-04 13:50:31 -0600

    Update team_gen.js

--- team_gen.js
+++ team_gen.js
@@ -3,6 +3,19 @@ function shuffle(o){
     return o;
 }
 
+function clear_all(){
+    for (var i = 1; i <= 20; i++){
+        t = document.getElementById('name_input'.concat(i));
+        t.value = "";
+    }
+    t = document.getElementById('team_list_two');
+    t.innerText = '';
+    t = document.getElementById('team_list_one');
+    t.innerText = '';
+    first_team_list = [];
+    odd_man_out = "";
+}
+
 var first_team_list = [];
 var odd_man_out = "";
 function second_gen(){
@@ -22,7 +35,7 @@ function generate_first(){
 }
 
 function generate_teams(oddmanout, first){
-  console.log('t', oddmanout);
+  //console.log('t', oddmanout);
   var players = [];
   for (var i=1;i <= 20; i++){
     var name = document.getElementById('name_input'.concat(i)).value;
@@ -34,32 +47,32 @@ function generate_teams(oddmanout, first){
   team_pairs = [];
   if(players.length%2 == 0){ // even
     for(var i=0; i < players.length; i+=2){
-      team_pairs.push(players[i] + ' ' + players[i+1]);
+      team_pairs.push(players[i] + ' & ' + players[i+1]);
     }
   } else { // odd number players
     if (oddmanout == false){ // first gen
       odd_man_out = players.pop();
-      team_pairs.push(odd_man_out + ' ' + players.pop());
-      team_pairs.push(odd_man_out + ' ' + players.pop());
+      team_pairs.push(odd_man_out + ' & ' + players.pop());
+      team_pairs.push(odd_man_out + ' & ' + players.pop());
       for(var i=0; i < players.length; i+=2){
-        team_pairs.push(players[i] + ' ' + players[i+1]);
+        team_pairs.push(players[i] + ' & ' + players[i+1]);
       }
       
     } else{ // second gen
       if (oddmanout != players[players.length-1]){
         var odd_man = players.pop();
-        team_pairs.push(odd_man + ' ' + players.pop());
-        team_pairs.push(odd_man + ' ' + players.pop());
+        team_pairs.push(odd_man + ' & ' + players.pop());
+        team_pairs.push(odd_man + ' & ' + players.pop());
         for(var i=0; i < players.length; i+=2){
-          team_pairs.push(players[i] + ' ' + players[i+1]);
+          team_pairs.push(players[i] + ' & ' + players[i+1]);
         }
       } else {
         shuffle(players);
         var odd_man = players.pop();
-        team_pairs.push(odd_man + ' ' + players.pop());
-        team_pairs.push(odd_man + ' ' + players.pop());
+        team_pairs.push(odd_man + ' & ' + players.pop());
+        team_pairs.push(odd_man + ' & ' + players.pop());
         for(var i=0; i < players.length; i+=2){
-          team_pairs.push(players[i] + ' ' + players[i+1]);
+          team_pairs.push(players[i] + ' & ' + players[i+1]);
         }
       }
     }
@@ -77,8 +90,8 @@ function check_teams_not_same(lista, listb){
     for (var j = 0; j < listb.length; j++){
       a = lista[i];
       b = listb[j];
-      anames = a.split(' ');
-      bnames = b.split(' ');
+      anames = a.split(' & ');
+      bnames = b.split(' & ');
       if((anames[0] == bnames[0] && anames[1] == bnames[1]) || (anames[1] == bnames[0] && anames[0] == bnames[1])){
         matches = true;
       }
